95ae048eb4b9c55c1998572aec76b8a994938bbf
Mirvitik <zombieboy3k@gmail.com>
2025-03-16 13:27:41 +0300

    Add files via upload

---
+++ lesson2/answer.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pluto_is_planet_and_cuba_is_free'
+cnt = 0
+
+
+@app.route('/galery', methods=['POST', 'GET'])
+def answer():
+    global cnt
+    if request.method == 'GET':
+        for el in os.walk(os.path.join(os.getcwd(), 'static\\img')):
+            images = [i for i in el[-1]]
+            break
+        return render_template('1.html', data=images)
+    if request.method == 'POST':
+        print('пост сука')
+        f = request.files['file']
+        print(f)
+        f.save(f'static/img/image{cnt}.png')
+        cnt += 1
+        for el in os.walk(os.path.join(os.getcwd(), 'static\\img')):
+            images = [i for i in el[-1]]
+            break
+        print(images)
+        return render_template('1.html', data=images)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
